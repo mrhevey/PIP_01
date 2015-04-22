50df599441de54be8cead725fb00ea84fe4b915c
mrhevey <mrhevey@student.rockvalleycollege.edu>
2015-04-21 21:16:39 -0500

    Picture Image Print 01
    Version 1 of Picture image Print

---
+++ PIP_01/PIPPhotoCell.swift
@@ -0,0 +1,16 @@
+//
+//  PIPPhotoCell.swift
+//  PIP_01
+//
+//  Created by mhevey on 4/21/15.
+//  Copyright (c) 2015 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class PIPPhotoCell: UICollectionViewCell {
+   
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+}
